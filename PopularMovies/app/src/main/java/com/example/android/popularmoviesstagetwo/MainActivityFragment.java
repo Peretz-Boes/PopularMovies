1dc435d285bea5db2a2afbd8d695f8c593dd7c14
Peretz Boes <peretzboes@gmail.com>
2017-01-18 20:57:42 -0500

    implement the settings

--- PopularMovies/app/src/main/java/com/example/android/popularmoviesstagetwo/MainActivityFragment.java
+++ PopularMovies/app/src/main/java/com/example/android/popularmoviesstagetwo/MainActivityFragment.java
@@ -7,6 +7,9 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -19,6 +22,7 @@ import com.example.android.popularmoviesstagetwo.model.Movie;
 import com.example.android.popularmoviesstagetwo.model.MoviesListResponse;
 import com.example.android.popularmoviesstagetwo.network.MovieClient;
 import com.example.android.popularmoviesstagetwo.network.MovieRetrofitInterface;
+import com.example.android.popularmoviesstagetwo.tasks.FetchFavouritesAsyncTask;
 import com.example.android.popularmoviesstagetwo.utils.Constants;
 
 import java.util.ArrayList;
@@ -36,6 +40,71 @@ public class MainActivityFragment extends Fragment {
     private MovieAdapter mMovieAdapter;
     TextView noMoviesTextView;
     private MovieRetrofitInterface movieRetrofitInterface;
+    private String sortCriteria= Constants.APIConstants.SORT_BY_POPULARITY;
+    private MenuItem menuItemSortByMostPopular;
+    private MenuItem menuItemSortByRating;
+    private MenuItem menuItemSortByFavourites;
+
+    public MainActivityFragment(){
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_main_fragment,menu);
+        menuItemSortByMostPopular=menu.findItem(R.id.action_sort_by_most_popular);
+        menuItemSortByRating=menu.findItem(R.id.action_sort_by_rating);
+        menuItemSortByFavourites=menu.findItem(R.id.action_sort_by_favourites);
+        if (sortCriteria.contentEquals(Constants.APIConstants.SORT_BY_POPULARITY)){
+            if (!menuItemSortByMostPopular.isChecked()){
+                menuItemSortByMostPopular.setChecked(true);
+            }
+        }else if (sortCriteria.contentEquals(Constants.APIConstants.SORT_BY_RATING)){
+            if (!menuItemSortByRating.isChecked()){
+                menuItemSortByRating.setChecked(true);
+            }
+        }else if (sortCriteria.contentEquals(Constants.APIConstants.SORT_BY_FAVOURITES)){
+            if (!menuItemSortByFavourites.isChecked()){
+                menuItemSortByFavourites.setChecked(true);
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.action_sort_by_most_popular:
+                if (isOnline()){
+                    sortCriteria=Constants.APIConstants.SORT_BY_POPULARITY;
+                    getMovies(sortCriteria);
+                    if (!menuItemSortByMostPopular.isChecked()){
+                        menuItemSortByMostPopular.setChecked(true);
+                    }
+                }
+                return true;
+            case R.id.action_sort_by_rating:
+                if (isOnline()){
+                    sortCriteria=Constants.APIConstants.SORT_BY_RATING;
+                    getMovies(sortCriteria);
+                    if (!menuItemSortByRating.isChecked()){
+                        menuItemSortByRating.setChecked(true);
+                    }
+                }
+                return true;
+            case R.id.action_sort_by_favourites:
+                sortCriteria=Constants.APIConstants.SORT_BY_FAVOURITES;
+                if (!menuItemSortByFavourites.isChecked()){
+                    menuItemSortByFavourites.setChecked(true);
+                }
+                new FetchFavouritesAsyncTask(getContext(),mMovieAdapter,movies).execute();
+                return true;
+            default:
+                return true;
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
         View rootView=inflater.inflate(R.layout.fragment_main,container,false);
